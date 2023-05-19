55caaff38f1c08de5af6fc20ac89a9ec9b848bfe
gznznzjsn <arik2046@gmail.com>
2023-05-19 11:21:03 +0300

    Fixed deserializer

--- src/main/java/com/gznznzjsn/inventoryservice/core/persistence/serializer/EquipmentDeserializer.java
+++ src/main/java/com/gznznzjsn/inventoryservice/core/persistence/serializer/EquipmentDeserializer.java
@@ -1,6 +1,5 @@
 package com.gznznzjsn.inventoryservice.core.persistence.serializer;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -9,12 +8,10 @@ import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.gznznzjsn.inventoryservice.core.model.Employee;
 import com.gznznzjsn.inventoryservice.core.model.Equipment;
 import com.gznznzjsn.inventoryservice.core.model.Inventory;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.UUID;
 
-@Component
 public class EquipmentDeserializer extends StdDeserializer<Equipment> {
 
     public EquipmentDeserializer(Class<?> vc) {
@@ -27,12 +24,12 @@ public class EquipmentDeserializer extends StdDeserializer<Equipment> {
         JsonNode node = codec.readTree(parser);
         return Equipment.builder()
                 .id(UUID.fromString(node.get("equipment_id").asText()))
-                .name(node.get("equipment_id").asText())
+                .name(node.get("equipment_name").asText())
                 .inventory(Inventory.builder()
-                        .id(UUID.randomUUID())
+                        .id(UUID.fromString(node.get("inventory_id").asText()))
                         .build())
                 .owner(Employee.builder()
-                        .id(UUID.randomUUID())
+                        .id(UUID.fromString(node.get("owner_id").asText()))
                         .build())
                 .build();
     }
