a5ea82d49d8670c07b16bfd8506fa5fe0b86c3d5
gznznzjsn <arik2046@gmail.com>
2023-05-22 10:17:41 +0300

    Extended Equipment in liquibase

--- src/main/resources/db/changelog/changeset/changeset-1.02-create-equipment.sql
+++ src/main/resources/db/changelog/changeset/changeset-1.02-create-equipment.sql
@@ -4,8 +4,10 @@
 create table equipment
 (
     equipment_id   uuid primary key,
-    inventory_id   uuid        not null references inventories on DELETE cascade,
-    equipment_name varchar(40) not null,
+    inventory_id   uuid         not null references inventories on DELETE cascade,
+    equipment_name varchar(40)  not null,
+    manufacturer   varchar(40)  not null,
+    description    varchar(255) not null,
     owner_id       uuid
 );
 --rollback drop table equipment;
